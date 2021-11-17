9f5daeda5f71df94b20fa62c1f3037edaee80a17
stefangamer <58218753+stefangamer@users.noreply.github.com>
2021-11-17 13:35:54 +0100

    Update Application.java

--- Checkpoint1/Exercise1/src/Application.java
+++ Checkpoint1/Exercise1/src/Application.java
@@ -7,38 +7,7 @@ public class Application implements Runnable {
     }
 
     public void run() {
-        int number = SaxionApp.getRandomValueBetween(1, 21);
-        int guess = 0;
-        int i = 0;
-        boolean gameOver = false;
-
-        SaxionApp.printLine("Guess the number <= 20, you have 7 guesses left.");
-
-        while (i < 7 && !gameOver) {
-            guess = SaxionApp.readInt();
-            if (guess == 69 || guess == 420) {
-                SaxionApp.printLine("Nice. You cheesed it.");
-                gameOver = true;
-            } else if (guess == 42) {
-                SaxionApp.printLine("No you dumbass, that's the answer to life universe and everything.");
-                SaxionApp.printLine("You have 7 guesses left.");
-                i++;
-            } else {
-                if (guess == number) {
-                    SaxionApp.printLine("You won! The number was: " + number + ". It took you " + (i + 1) + " guesses.");
-                    gameOver = true;
-                } else {
-                    if (i < 6) {
-                        SaxionApp.printLine("Try again. You have " + (6 - i) + " guesses left.");
-                    }
-                    i++;
-                }
-            }
-        }
-
-        if (!gameOver) {
-            SaxionApp.printLine("Game Over. The number was " + number + ".");
-        }
+        SaxionApp.printLine("Hoi");
     }
 
 }
